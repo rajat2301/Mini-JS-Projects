44bbe2a0caa3cb08934a907ffa18b31160adf9b6
rajat2301 <rajatbansal2301@gmail.com>
2022-02-19 19:17:07 +0530

    new

---
+++ Counter/script.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach((counter)=>{
+    counter.innerHTML=0;
+
+    const updateCounter = () => {
+        const startingCount = +counter.innerHTML;
+        const targetCount = +counter.getAttribute('data-target');
+        const incr = targetCount/200;
+        if(startingCount < targetCount)
+        {
+            counter.innerHTML = `${startingCount + incr}`;
+        }
+        setTimeout(updateCounter,30);
+    }
+
+    updateCounter();
+
+})
